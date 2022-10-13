ca4acdf1d24e9b1a0d25670fc7444f90a6679279
Joel tondozi <joeltondozi@gmail.com>
2022-10-13 19:45:14 +0200

    feat : message card

---
+++ Frontend/biso-client/src/Component /Message.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Message = () => {
+    return (
+        <div className='Message-container'>
+            <p className='Message-content'></p>
+        </div>
+    );
+};
+
+export default Message;
